be20c64e97c8c6ad8d62a0e75eb058c95072d357
Amit Azulay <azulay7@gmail.com>
2023-07-20 10:06:13 +0300

    rm compnents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,19 +12,11 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatIconModule } from '@angular/material/icon';
 import { MatRadioModule } from '@angular/material/radio';
-import { TextInputComponent } from './dynamic-components/text-input/text-input.component';
-import { CheckboxComponent } from './dynamic-components/checkbox/checkbox.component';
-import { RadioComponent } from './dynamic-components/radio/radio.component';
-import { SelectComponent } from './dynamic-components/select/select.component';
 import {MatCheckboxModule} from '@angular/material/checkbox';
 @NgModule({
   declarations: [
     AppComponent,
-    DynamicFormComponent,
-    TextInputComponent,
-    CheckboxComponent,
-    RadioComponent,
-    SelectComponent
+    DynamicFormComponent
   ],
   imports: [
     BrowserModule,
